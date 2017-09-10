6a6aaa89f93651a859329adece1e5af8abf41c8f
krishnakumar98 <KRISHNAshanmugamcse2015@gmail.com>
2017-09-10 16:34:46 +0530

    Add files via upload

---
+++ gre.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	 int a,b,c;
+	 scanf("%d%d%d",&a,&b,&c);
+		 if(a>b&&a>c)
+		 {
+			 printf("%d is greatest no",a);
+		 }
+	 else if(b>a&&b>c)
+	 {
+		  printf("%d is greatest no",b);
+	 }
+	 else
+		  printf("%d is greatest no",c);
+}
