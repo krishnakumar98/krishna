cc953dbf866b8a709e6c53a63a5b139eb6e631ae
krishnakumar98 <KRISHNAshanmugamcse2015@gmail.com>
2017-09-14 14:56:53 +0530

    Update caln.c

--- caln.c
+++ caln.c
@@ -5,5 +5,5 @@ void main()
   printf("\nEnter the value for sum of natural no's");
   scanf("%d",&n);
   s=(n*(n+1))/2;
-  printf("sum of %d number is",n,s);
+  printf("sum of %d number is %d",n,s);
  }
