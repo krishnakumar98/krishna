54c7358d375e3908330b50f1ca14f8fe3b3dd3e4
krishnakumar98 <KRISHNAshanmugamcse2015@gmail.com>
2017-09-23 11:06:43 +0530

    Create GCD.java

---
+++ GCD.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class GCD {
+	static int gcd(int x,int y)
+	{
+		int temp,r;
+		if(x<y)
+		{
+			temp=x;
+			x=y;
+			y=temp;
+		}
+		r=x%y;
+		if(r==0)
+			return y;
+		else
+		{	
+		x=y;
+		y=r;
+		gcd(x,y);
+		}
+		return y;
+		    
+		
+	}
+	public static void main(String[] args) {
+	int t,a,b;
+	Scanner sc=new Scanner(System.in);
+	System.out.println("\nEnter two values");
+	a=sc.nextInt();
+	b=sc.nextInt();
+	t=gcd(a,b);
+	System.out.println("the gcd of "+a+" and "+b+" is "+t);
+	sc.close();
+	}
+}
