470024b66bb801a7d06044909732050e54034fa6
krishnakumar98 <KRISHNAshanmugamcse2015@gmail.com>
2017-09-24 21:45:09 +0530

    Create ASCII.java

---
+++ ASCII.java
@@ -0,0 +1,14 @@
+public class ASCII {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       char ch=0;
+       int i;
+       for(i=0;i<256;i++)
+       {
+       System.out.println(ch);
+       ch++;
+       }		   
+	}
+
+}
