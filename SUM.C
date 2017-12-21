8c9ff3e15164f2dd2f9d6426652275791b2d3aaf
krishnakumar98 <KRISHNAshanmugamcse2015@gmail.com>
2017-12-21 13:56:51 +0000

    Update SUM.C

--- SUM.C
+++ SUM.C
@@ -1,10 +1,12 @@
 #include<stdio.h>
 void main()
 {
- int n,s=0,i;
- printf("\nEnter the values for n");
- scanf("%d",&n);
- for(i=1;i<=n;i++)
-    s=s+i;
- printf("\nsum of %d no is %d",n,s);
+ int n,k,a[100],s=0,i;
+ printf("\nEnter the values for n and k");
+ scanf("%d%d",&n,&k);
+ for(i=0;i<n;i++)
+    scanf("%d",&a[i]);
+ for(i=0;i<k;i++)
+    s=s+a[i];
+ printf("\nsum of %d no in the given array  is %d",k,s);
 }
