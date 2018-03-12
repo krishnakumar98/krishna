c47672a4516fc65b23298c07efb8039bc41cce7b
krishnakumar <KRISHNAshanmugamcse2015@gmail.com>
2018-03-12 19:09:18 +0530

    Create Inc.java

---
+++ Inc.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Inc {
+    public static void main(String args[]) {
+        int n,count=0;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        n++;
+        System.out.println(n);
+    }
+}
