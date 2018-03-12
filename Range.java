a6ae125f5855f05c36c5677d25dad7a0b31548d5
krishnakumar <KRISHNAshanmugamcse2015@gmail.com>
2018-03-12 18:55:22 +0530

    Create Range.java

---
+++ Range.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Range {
+    public static void main(String args[]) {
+        int a;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        if(a>=1&&a<=10)
+        {
+            System.out.println("yes");
+        }
+        else
+        {
+            System.out.println("no");
+        }
+    }
+}
