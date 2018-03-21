d8868b8ba85ba6803c9f5bd1ae46c6f53a9304d6
krishnakumar <KRISHNAshanmugamcse2015@gmail.com>
2018-03-21 20:48:33 +0530

    Create noorder.py

---
+++ noorder.py
@@ -0,0 +1,14 @@
+a=int(input("Enter the number"))
+print("Enter the elements")
+j=1
+li=[]
+2
+for i in range(0,a):
+	b=int(input())
+	li.append(b)
+	if(j==b):
+		j=j+1
+	else:
+		c=j
+		j=j+1
+print(c)
