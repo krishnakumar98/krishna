7ea87578e59a24ff19bb3e5058c48312ac9dddde
krishnakumar <KRISHNAshanmugamcse2015@gmail.com>
2018-03-21 19:42:34 +0530

    Create special.c

---
+++ special.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+char str[50],i,c=0;
+printf("enter the string");
+scanf("%s",str);
+for(i=0;str[i]!='\0';i++)
+{
+if((str[i]>='0')&&(str[i]<='9')||(str[i]>='a')&&(str[i]<='z'))
+{
+continue;
+}
+else
+{
+    c++;
+}
+}
+printf("%d",c);
+}
