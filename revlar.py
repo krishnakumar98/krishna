e17c4986408c5b68f6ac62257ee4d220159face5
krishnakumar <KRISHNAshanmugamcse2015@gmail.com>
2018-03-23 08:31:55 +0530

    Update revlar.py

--- revlar.py
+++ revlar.py
@@ -1,11 +1,9 @@
 n=int(input("Enter a number"))
 li=[]
-r=0
-while(n>0):
-	n1=n%10
-	li.append(n1)
-	n=int(n/10)
+r=""
+for i in range(0,n):
+	li.append(int(input()))
 a=sorted(li)
 for i in range(len(a)-1,-1,-1):
-	r=r*10+a[i]
-print(r)	
+	r=r+str(a[i])
+print(int(r))	
